8e1430dfd8b391114b9691463bd911b1eb88b9c2
KiranJungGurung <kirangurung394@gmail.com>
2023-01-25 08:13:39 +0545

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Kiran Gurung on 25/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
